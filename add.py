e5a42301b8f19ad869c773725b5a69c8892db226
Ketzalli Sanchez <A01786306@tec.mx>
2023-08-15 12:30:57 -0600

    pagina geb

---
+++ add.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+def hello():
+    """Print"hello world" and return none"""
+    print("hello world")
+
+# noin program starts here
+hello()
